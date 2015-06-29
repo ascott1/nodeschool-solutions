91db4e871970cfe0fbc713e9e4062fe89d8c7c65
Adam Scott <adam.scott@cfpb.gov>
2015-06-29 15:58:06 -0400

    props from server

--- learnyoureact/views/index.jsx
+++ learnyoureact/views/index.jsx
@@ -6,26 +6,26 @@ var style = {
    }
  };
 
-var TodoBox = React.createClass({
-  render: function() {
-    return (
-      <div className="todoBox">
-        <h1>Todos</h1>
-        <TodoList />
-        <TodoForm />
-      </div>
-    );
-  }
-});
+ var TodoBox = React.createClass({
+   render: function() {
+     return (
+       <div className="todoBox">
+         <h1>Todos</h1>
+         <TodoList data = {this.props.data} />
+         <TodoForm />
+       </div>
+     );
+   }
+ });
 
 var TodoList = React.createClass({
   render: function() {
+    var todo = this.props.data.map(function(obj) { return <Todo title={obj.title} key={obj.title}>{obj.detail}</Todo>});
     return (
       <div className="todoList">
         <table style={{border: "2px solid black"}}>
           <tbody>
-            <Todo title="Shopping">Milk</Todo>
-            <Todo title="Hair cut">13:00</Todo>
+            {todo}
           </tbody>
         </table>
       </div>
