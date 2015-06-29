e585e6e058afbdea3b18f86d8d7ba422f9d34e2d
Adam Scott <adam.scott@cfpb.gov>
2015-06-29 16:32:40 -0400

    css2

--- learnyoureact/program.js
+++ learnyoureact/program.js
@@ -1,6 +1,3 @@
-var express = require('express');
-var app = express();
-
 var React = require('react');
 var DOM = React.DOM;
 var body = DOM.body;
@@ -8,6 +5,9 @@ var div = DOM.div;
 var script = DOM.script;
 
 var browserify = require('browserify');
+
+var express = require('express');
+var app = express();
 
 app.set('port', (process.argv[2] || 3000));
 app.set('view engine', 'jsx');
@@ -18,14 +18,8 @@ require('node-jsx').install();
 var TodoBox = require('./views/index.jsx');
 
 var data = [
-  {
-    title: 'Shopping',
-    detail: process.argv[3]
-  },
-  {
-    title: 'Hair cut',
-    detail: process.argv[4]
-  }
+    { title: 'Shopping', detail: process.argv[3] },
+    { title: 'Hair cut', detail: process.argv[4] }
 ];
 
 app.use('/bundle.js', function(req, res) {
@@ -37,21 +31,21 @@ app.use('/bundle.js', function(req, res) {
 });
 
 app.use('/', function(req, res) {
-   var initialData = JSON.stringify(data);
-   var markup = React.renderToString(React.createElement(TodoBox, {data: data}));
+  var initialData = JSON.stringify(data);
+  var markup = React.renderToString(React.createElement(TodoBox, {data: data}));
 
-   res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Content-Type', 'text/html');
 
-   var html = React.renderToStaticMarkup(body(null,
-       div({id: 'app', dangerouslySetInnerHTML: {__html: markup}}),
-       script({id: 'initial-data',
-               type: 'text/plain',
-               'data-json': initialData
-             }),
-       script({src: '/bundle.js'})
-   ));
+  var html = React.renderToStaticMarkup(body(null,
+      div({id: 'app', dangerouslySetInnerHTML: {__html: markup}}),
+      script({id: 'initial-data',
+              type: 'text/plain',
+              'data-json': initialData
+            }),
+      script({src: '/bundle.js'})
+  ));
 
-   res.end(html);
- });
+  res.end(html);
+});
 
 app.listen(app.get('port'), function() {});
--- learnyoureact/views/index.jsx
+++ learnyoureact/views/index.jsx
@@ -1,35 +1,29 @@
 var React = require('react');
 
-var style = {
-   tableContent: {
-     border: "1px solid black"
-   }
- };
-
- var TodoBox = React.createClass({
-   render: function() {
-     return (
-       <div className="todoBox">
-         <h1>Todos</h1>
-         <TodoList data = {this.props.data} />
-         <TodoForm />
-       </div>
-     );
-   }
- });
+var TodoBox = React.createClass({
+  render: function() {
+      return (
+        <div className="todoBox">
+          <h1>Todos</h1>
+            <TodoList data = {this.props.data} />
+            <TodoForm />
+        </div>
+      );
+  }
+});
 
 var TodoList = React.createClass({
   render: function() {
     var todo = this.props.data.map(function(obj) { return <Todo title={obj.title} key={obj.title}>{obj.detail}</Todo>});
     return (
-      <div className="todoList">
+      <div className = "todoList">
         <table style={{border: "2px solid black"}}>
           <tbody>
             {todo}
           </tbody>
         </table>
       </div>
-    )
+    );
   }
 });
 
@@ -37,38 +31,57 @@ var Todo = React.createClass({
   propTypes: {
     title: React.PropTypes.string.isRequired
   },
-
-  getInitialState: function() {
+  getInitialState: function () {
     return {
-      checked: false
+      checked: false,
+      TodoStyle: style.notCheckedTodo
+    };
+  },
+  handleChange: function (e) {
+    this.setState({
+      checked: e.target.checked
+    });
+    if (e.target.checked) {
+      this.setState({
+        TodoStyle: style.checkedTodo
+      });
+    } else {
+      this.setState({
+        TodoStyle: style.notCheckedTodo
+      });
     }
   },
-
-  handleChange: function() {
-    this.setState({
-      checked: true
-    })
-  },
-
-  render: function() {
+  render: function () {
     return (
-      <tr>
+      <tr style={this.state.TodoStyle}>
         <td style={style.tableContent}><input type="checkbox" checked={this.state.checked} onChange={this.handleChange} /></td>
         <td style={style.tableContent}>{this.props.title}</td>
         <td style={style.tableContent}>{this.props.children}</td>
       </tr>
-    )
+    );
   }
 });
 
 var TodoForm = React.createClass({
   render: function() {
     return (
-      <div className="todoForm">
+      <div className = "todoForm">
         I am a TodoForm.
       </div>
-    )
+    );
   }
 });
+
+var style = {
+  checkedTodo: {
+    textDecoration: "line-through"
+  },
+  notCheckedTodo: {
+    textDecoration: "none"
+  },
+  tableContent: {
+    border: "1px solid black"
+  }
+};
 
 module.exports = TodoBox;
